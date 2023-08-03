3e8d2c223a2146add0440ff3f512bfcf145ae65b
AntoCoder13 <antonellaargisanchez@gmail.com>
2023-08-03 19:53:19 -0300

    Alerta dulce agregada

--- index-new.js
+++ index-new.js
@@ -1,3 +1,25 @@
+//BOTON PARA CAMBIAR EL FONDO DE COLOR
+const boton = document.querySelector('button');
+const color = document.getElementById('color');
+
+function generarColorAleatorio(){
+    let digitos = '0123456789ABCDEF';
+    let colorHex = '#';
+
+    for( let i=0; i<6; i++){
+        let indiceAleatorio = Math.floor(Math.random() *16); 
+        colorHex += digitos[indiceAleatorio];
+    }
+
+    return colorHex;
+}
+
+boton.addEventListener('click', function(){
+    let colorAleatorio = generarColorAleatorio(); 
+    color.textContent = colorAleatorio;
+    document.body.style.backgroundColor = colorAleatorio;
+});
+
 // Obtener referencias a los elementos del formulario y el div de la lista de turnos
 const form = document.getElementById('appointment-form');
 const nameInput = document.getElementById('name');
@@ -48,3 +70,39 @@ window.addEventListener('load', function() {
         timeInput.value = parsedData.time;
     }
 });
+
+document.getElementById('boton_submit').addEventListener('click', ( => {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false,
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Estás segur@ de tu turno?',
+        text: "Si lo aceptás quedará asignado!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, lo quiero!',
+        cancelButtonText: 'No, lo pensaré mejor!',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            'Aceptado!',
+            'Te esperamos para tu turno, gracias por elegirnos',
+            'success'
+          )
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Intenta de nuevo la próxima!',
+            'error'
+          )
+        }
+      })
+}))
